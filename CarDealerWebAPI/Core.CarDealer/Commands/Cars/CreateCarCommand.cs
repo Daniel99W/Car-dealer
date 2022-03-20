4608588f349e81c1e4ec9814936a123362a87667
Costache Daniel <daniel.costache11@gmail.com>
2022-03-20 21:57:41 +0200

    Added GetCarByUserIdQuery Added GetCarsByBrandType Added GetCarByUserIdHandler Added GetCarsByBrandTypeHandler Added Repository abstract class

---
+++ CarDealerWebAPI/Core.CarDealer/Commands/Cars/CreateCarCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CarDealer.Commands
+{
+    public class CreateCarCommand : IRequest<bool>
+    {
+        public string CarNumber { get; set; }
+
+    }
+}
---
+++ CarDealerWebAPI/Core.CarDealer/CommandsHandler/Cars/CreateCarCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Core.CarDealer.Commands;
+using Core.CarDealer.Interfaces;
+using Core.CarDealer.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CarDealer.CommandsHandler.Cars
+{
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand,bool>
+    {
+        private IRepository<Car> _carRepository;
+
+        public CreateCarCommandHandler(IRepository<Car> carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public async Task<bool> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            _carRepository.Create(new Car 
+            { 
+                CarNumber = request.CarNumber
+            });
+
+            await _carRepository.SaveChangesAsync();
+
+            return true;
+        }
+
+    }
+}
