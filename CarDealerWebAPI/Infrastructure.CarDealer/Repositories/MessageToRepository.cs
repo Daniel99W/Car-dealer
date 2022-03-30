319a5ca407ac34aa883874a52bc67be8734bbd65
Costache Daniel <daniel.costache11@gmail.com>
2022-03-30 15:13:22 +0300

    Modified the name of the query for the car

---
+++ CarDealerWebAPI/Infrastructure.CarDealer/Repositories/MessageToRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.CarDealer.Interfaces;
+using Core.CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.CarDealer.Repositories
+{
+    public class MessageToRepository : Repository<MessageTo>,IRepositoryMessageTo
+    { 
+        public MessageToRepository(AnnouncesContext announcesContext)
+            :base(announcesContext)
+        {
+        }
+    }
+}
