a3eca0f618a2195d906863f6f8629937ccb84b85
Costache Daniel <daniel.costache11@gmail.com>
2022-04-03 16:45:53 +0300

    Added CreateUnitOfWorkMessageCommand and queries for getting messages by senderId and receiverId

---
+++ CarDealerWebAPI/Core.CarDealer/Commands/Mesages/CreateUnitOfWorkMessagesCommand.cs
@@ -0,0 +1,19 @@
+﻿using Core.CarDealer.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CarDealer.Commands.Messages
+{
+    public class CreateUnitOfWorkMessagesCommand : IRequest<Message>
+    {
+        public int UserId { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public int ReceiverId { get; set; }
+    }
+}
--- CarDealerWebAPI/Core.CarDealer/Queries/Messages/GetMessagesBySenderReceiverQuery.cs
+++ CarDealerWebAPI/Core.CarDealer/Queries/Messages/GetMessagesBySenderReceiverQuery.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Core.CarDealer.Commands.Cars
+namespace Core.CarDealer.Queries.Messages
 {
-    internal class CreateUnitOfWorkMessages
+    internal class GetMessagesBySenderIdReceiverId
     {
     }
 }
