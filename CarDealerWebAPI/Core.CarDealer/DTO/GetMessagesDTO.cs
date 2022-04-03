930f165cb7d9d28e25d2093b101cea42784e6611
Costache Daniel <daniel.costache11@gmail.com>
2022-04-03 23:06:18 +0300

    Added GetMessagesDTO

---
+++ CarDealerWebAPI/Core.CarDealer/DTO/GetMessagesDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CarDealer.DTO
+{
+    public class GetMessagesDTO
+    {
+        public int ReceiverId { get; set; }
+        public int SenderId { get; set; }
+    }
+}
