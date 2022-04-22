84db047ace849d4c61061cf8b0afe5119068e6a3
Costache Daniel <daniel.costache11@gmail.com>
2022-04-22 04:32:39 +0300

    Utilities File

---
+++ CarDealerWebAPI/Core.CarDealer/Utilities/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CarDealer.Utilities
+{
+    public static class Utilities
+    {
+        public static IFormFile CreateImageWithNewName(IFormFile formFile,Guid carId)
+        {
+            string fileName = formFile.FileName[..formFile.FileName.IndexOf('.')];
+            Console.WriteLine(fileName);
+            string imageName = fileName + carId.ToString();
+            FormFile newFormFile = new FormFile(formFile.OpenReadStream(), 0, formFile.Length,
+                imageName, imageName);
+
+            return newFormFile;
+        }
+    }
+}
